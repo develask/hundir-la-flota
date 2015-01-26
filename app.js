4cd2518aae9aa159287392b4e043525246488a38
develask <develascomikel@gmail.com>
2015-01-26 22:06:37 +0100

    Lehenengo egoera

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
 var server = app.listen(8080, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+});
 
+app.use(express.static(__dirname + '/public'));
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+//        io.sockets.emit('message', data);
+        console.log(data);
+    });
+    socket.on("disconnect", function () {
+//        io.sockets.emit("user disconnected");
+        console.log("user disconnect");
+    });
 });
