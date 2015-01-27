259b7503962356ea49ea0cfd9f324c2d05809721
develask <develascomikel@gmail.com>
2015-01-27 15:54:18 +0100

    Index.html cambiado

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,12 @@
-var socket = io('http://localhost:8080');
-socket.on('message', function (data) {
-    console.log(data);
-    socket.emit('send', { my: 'data' });
+//var socket = io('http://localhost:8080');
+//socket.on('message', function (data) {
+//    console.log(data);
+//    socket.emit('send', { my: 'data' });
+//});
+
+$("#signin").on("click", function(ev){
+    $('#signindiv').modal('show');
+});
+$("#comosejuega").on("click", function(ev){
+    $('#instrucciones').modal('show');
 });
