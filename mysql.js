b04e35c1304aec57056824d3dd80c6ad14e738dd
develask <develascomikel@gmail.com>
2015-01-28 16:52:08 +0100

    mysql connection made

--- mysql.js
+++ mysql.js
@@ -1,60 +1,38 @@
 // http://www.thegeekstuff.com/2014/01/mysql-nodejs-intro/
 
 var mysql = require('mysql');
-var fs = require('fs');
 var crypto = require('crypto');
-
-var user;
-var password;
-fs.readFile('./.params.txt', {encoding: 'utf8'}, function (err, data) {
-  if (err) throw err;
-    data = data.split('\n');
-    for(var each in data){
-        var l = data[each].split(":");
-        if (l[0] == "user") user =  l[1];
-        if (l[0] == "password") password =  l[1];
-    }
-    console.log(user, password);
     
-    var connection =  mysql.createConnection({
-        host : "127.0.0.1",
-        user : user,
-        password: password
-    });
-
-    connection.connect();
-
-    connection.query("use hundirlaflota");
-    //  var strQuery = “select * from table1”;	
-    //  
-    //  connection.query( strQuery, function(err, rows){
-    //  	if(err)	{
-    //  		throw err;
-    //  	}else{
-    //  		console.log( rows );
-    //  	}
-    //  });
-    function newUsuario(nombre, contraseña, email){
-        var shasum = crypto.createHash('sha1');
-        shasum.update(contraseña+nombre);
-        connection.query("INSERT INTO hundirlaflota.users (`nombre`,`contraseña_hash`,`email`) values ('"+nombre+"','"+shasum.digest('hex')+"','"+email+"')", function(err, rows){
-            if(err) throw err;
-        });
-    }
-    
-    function signIn(user, password, callback){
-        var shasum = crypto.createHash('sha1');
-        shasum.update(password+user);
-        connection.query("SELECT * FROM hundirlaflota.users WHERE nombre='"+user+"' AND contraseña_hash='"+shasum.digest('hex')+"'", function(err, rows){
-            if(err) throw err;
-            if (rows.length == 1){
-                callback(true);
-            }else{
-                callback(false);
-            }
-        });
-    }
-    
-    module.exports.newUsuario = newUsuario;
-    module.exports.signIn = signIn;
+var connection =  mysql.createConnection({
+    host : "176.84.103.209",
+    user : "hundir",
+    password: "laflota"
 });
+
+connection.connect();
+
+connection.query("use hundirlaflota");
+
+function newUsuario(nombre, contraseña, email){
+    var shasum = crypto.createHash('sha1');
+    shasum.update(contraseña+nombre);
+    connection.query("INSERT INTO hundirlaflota.users (`nombre`,`contraseña_hash`,`email`) values ('"+nombre+"','"+shasum.digest('hex')+"','"+email+"')", function(err, rows){
+        if(err) throw err;
+    });
+}
+
+function signIn(user, password, callback){
+    var shasum = crypto.createHash('sha1');
+    shasum.update(password+user);
+    connection.query("SELECT * FROM hundirlaflota.users WHERE nombre='"+user+"' AND contraseña_hash='"+shasum.digest('hex')+"'", function(err, rows){
+        if(err) throw err;
+        if (rows.length == 1){
+            callback(true);
+        }else{
+            callback(false);
+        }
+    });
+}
+
+module.exports.newUsuario = newUsuario;
+module.exports.signIn = signIn;
