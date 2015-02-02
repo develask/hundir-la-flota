e1fa43537c6d687335378928d11fe2e6b99216a5
JorgeNieto <jnieto011@ikasle.ehu.es>
2015-02-02 23:06:43 +0100

    Aldaketak .sh

--- change_IP.sh
+++ change_IP.sh
@@ -4,3 +4,5 @@ sed -e "s/localhost/$var1/g" ./app.js > op.txt
 mv op.txt app.js
 sed -e "s/localhost/$var1/g" ./public/js/index.js > op.txt
 mv op.txt public/js/index.js
+sed -e "s/localhost/$var1/g" ./mail.js > op.txt
+mv op.txt index.js
