713e7c7e79a02aad6484f1cf28cbc350393bf597
develask <develascomikel@gmail.com>
2015-02-06 15:54:57 +0100

    Joku errealean implementatu behar da

--- hundirlaflota.js
+++ hundirlaflota.js
@@ -10,12 +10,13 @@ function Tablero(length){
 				barco: false,
 				pulsado: false
 			});
-            html += "<td id='"+i+j+"'>__</td>"; 
+            html += "<td id='"+i+"-"+j+"' class='cuadrado'>__</td>"; 
 		};
         html += "</tr>";
 		tablero.push(li);
 	};
     tt.innerHTML = html;
+    
 	this.impr = function(){
 		for (var i = 0; i < tablero.length; i++) {
 			var fila = tablero[i];
@@ -23,7 +24,7 @@ function Tablero(length){
 			for (var c = 0; c < fila.length; c++) {
 				var t = fila[c];
 				linea += " "+(t.barco?"1":"0") + " - " + (t.pulsado?"1":"0")+" |";
-                document.getElementById(i+""+c).innerHTML = (t.barco?"*":"_")+(t.pulsado?"X":"_")
+                document.getElementById(i+"-"+c).innerHTML = (t.barco?"*":"_")+(t.pulsado?"X":"_")
 			};
 			//console.log(linea);
 		};
@@ -153,3 +154,73 @@ tablero.newBarco([5,0],false, 4);
 tablero.newBarco([1,2],false, 3);
 tablero.newBarco([4,4],true, 1);
 tablero.impr();
+var cuadrados = document.getElementsByClassName('cuadrado');
+for(var ind = 0; ind < cuadrados.length; ind++){
+    var cuadrado = cuadrados[ind];
+    cuadrado.addEventListener("click", function(ev){
+        var id = this.getAttribute("id");
+        id = id.split("-");
+        var r = tablero.pulsar(id[1],id[0]);
+        if (r == 0){
+            alert("Agua");
+        }else if(r == 1){
+            alert("Tocado");
+        }else{
+            alert("Hundido")
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+var canvas1 = document.getElementById("micanvas1");
+var canvas2 = document.getElementById("micanvas2");
+if (canvas1.getContext && canvas1.getContext) {
+    var ctx1 = canvas1.getContext("2d");
+    var ctx2 = canvas2.getContext("2d");
+    for(var x=0;x<10;x++){
+        for(var y=0;y<10;y++){
+            if((x+y)%2==0){
+                ctx1.fillStyle = "rgb(200,0,0)";
+                ctx1.fillRect (x*40,y*40,(x+1)* 40,(y+1)*40);
+                ctx2.fillStyle = "rgb(200,0,0)";
+                ctx2.fillRect (x*40,y*40,(x+1)* 40,(y+1)*40);
+            }else{
+                ctx1.fillStyle = "rgb(255, 255, 255)";
+                ctx1.fillRect (x*40,y*40,(x+1)* 40,(y+1)*40);
+                ctx2.fillStyle = "rgb(255, 255, 255)";
+                ctx2.fillRect (x*40,y*40,(x+1)* 40,(y+1)*40);
+            }
+        }
+    }
+}
+var canvas1 = document.getElementById("micanvas1");
+var ctx1 = canvas1.getContext("2d");
+for (var i=40;i<400;i=i+40){
+    ctx1.moveTo(i,0);
+    ctx2.lineTo(i,400);
+}
+for (var i=40;i<400;i=i+40){
+    ctx1.moveTo(0,i);
+    ctx2.lineTo(0,i);
+}
+ctx1.strokeStyle = "#f00";
+ctx1.stroke();
+$("#micanvas1").on("click", function(ev){
+    var canvas1 = document.getElementById("micanvas1");
+    var x1 = parseInt((ev.clientX-canvas1.offsetLeft)/40);
+    var y1 = parseInt((ev.clientY-canvas1.offsetTop)/40);
+    alert(x1 + "  " + y1);
+});
+$("#micanvas2").on("click", function(ev){
+    var canvas2 = document.getElementById("micanvas2");
+    var x2 = parseInt((ev.clientX-canvas2.offsetLeft)/40);
+    var y2 = parseInt((ev.clientY-canvas2.offsetTop)/40);
+    alert(x2 + "  " + y2);
+});
